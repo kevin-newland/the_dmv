fcdb67823bb4a204b08a554a9505160c1c2589c3
Kevin Newland <kevin.newland@yahoo.com>
2024-10-18 20:09:11 -0500

    tests to facility_spec

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
-
+require 'pry'
 RSpec.describe Facility do
   before(:each) do
     @facility = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
@@ -24,15 +24,18 @@ RSpec.describe Facility do
     end
   end
 
-  before(:each) do
-    @facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
-    @facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
-    @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
-    @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
-    @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
-  end
+
 
   describe '#initialize add functionality' do
+    before(:each) do
+      @facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+      @facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
+      @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+      @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+      @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+      @today = Date.today
+    end
+
     it "initialize set up" do
       expect(@facility_1).to be_an_instance_of(Facility)
       expect(@facility_2).to be_an_instance_of(Facility)
@@ -45,13 +48,34 @@ RSpec.describe Facility do
       expect(@facility_1.services).to eq([])
 
       @facility_1.add_service('Vehicle Registration')
-
       expect(@facility_1.services).to include('Vehicle Registration')
     end
 
-    it 'can register vehicles' do
-      
+    it 'can start not registered with zero collected fees' do
+      @facility_1.add_service('Vehicle Registration')
+
+      expect(@cruz.registration_date).to be(nil)
+      expect(@facility_1.registered_vehicles).to eq([])
+      expect(@facility_1.collected_fees).to eq(0)
+      # @facility_1.register_vehicle(@cruz)
     end
+
+    it 'can register vehicle @cruz' do 
+      @facility_1.register_vehicle(@cruz)
+
+      expect(@facility_1.register_vehicle(@cruz)).to be true
+      expect(@cruz.plate_type).to be(:regular)
+      expect(@cruz.registration_date).to eq(@today)
+      expect(@facility_1.collected_fees).to eq(100)
+    end
+    # it '@cruz can have registration_date' do
+    #   # @cruz.registration_date
+    #   expect(@cruz.registration_date).to eq(@today)
+    # end
+
+    # xit 'can collect fees from cruz registration' do
+    #   expect(@facility_1.collected_fees).to eq(100)
+    # end
 
 
   end
