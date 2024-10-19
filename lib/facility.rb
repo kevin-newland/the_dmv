89d4687d8a1a4c64f8aab9a7a25ba70e22c5869c
Kevin Newland <kevin.newland@yahoo.com>
2024-10-18 20:08:05 -0500

    changes to facility class

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,39 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, 
+              :address, 
+              :phone, 
+              :services, 
+              :registered_vehicles, 
+              :collected_fees
+              
 
   def initialize(facilities_details)
     @name = facilities_details[:name]
     @address = facilities_details[:address]
     @phone = facilities_details[:phone]
     @services = []
+    @registered_vehicles = []
+    @collected_fees = 0
+    # binding.pry
   end
 
   def add_service(service)
     @services << service
+  end
+
+  def register_vehicle(vehicle)
+    vehicle.set_registration_date
+    true
+  end
+
+  def plate_type 
+    if @vehicle.year > 25
+      :antique
+    elsif @vehicle.year == 12
+      :regular
+    elsif @vehicle.year == 5
+      :ev
+
+    end
   end
 end
