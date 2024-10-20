221556c9d332830859ba9cf32ad1049e5412b4dc
Kevin Newland <kevin.newland@yahoo.com>
2024-10-20 16:11:03 -0500

    add create_vehicles to vehicle_factory class

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,3 +1,17 @@
 class Vehicle_factory
-  
+
+  def initialize
+    @new_vehicles = []
+    @engine = :ev
+  end
+  def create_vehicles(wa_ev_registrations)
+    wa_ev_registrations.map do |wa_ev_registrations|
+      @new_vehicles << (
+          @engine = :ev
+          
+
+
+      )
+    end
+  end
 end
