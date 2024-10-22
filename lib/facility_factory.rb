ea5d0a865da13d431605d60b33c364efdddd99e8
Kevin Newland <kevin.newland@yahoo.com>
2024-10-22 15:33:33 -0500

    add changes to make create_facilities method account for all data sets

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -3,16 +3,17 @@ class FacilityFactory
     attr_reader :new_facilities
 
   def initialize(new_facilities = [])
-    @new_facilities = []
+    @new_facilities = new_facilities
   end
 
 
-  def create_facilities(co_dmv_office_locations)
-    co_dmv_office_locations.each do |facility|  
-      @facility = FacilityFactory.new(facility) 
+  def create_facilities(all_office_locations)
+    all_office_locations.each do |facility|  
+      facility = Facility.new(facility) 
       
       @new_facilities << facility
-      binding.pry
+     
     end
+    # binding.pry
   end
 end
